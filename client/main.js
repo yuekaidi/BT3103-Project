e4b51185391db2b7537e187923d86a7a07f3b891
Chen Yanran <chenyanranclarissa@gmail.com>
2016-10-23 17:45:44 +0800

    completed: create a sample order. update corresponding customer's order history.

--- client/main.js
+++ client/main.js
@@ -7,3 +7,46 @@ import { Order, Mmeber, Coupon, Menu } from '../api/database.js';
 import './index.html';
 import './login.html';
 import '../lib/routing.js';
+
+
+//to store the created order
+var createdOrderId = new ReactiveVar(null);
+
+Template.order.helpers({
+    'createdOrder': function(){
+    	return Orders.findOne({ _id: createdOrderId });
+    }
+
+})
+
+
+Template.order.events({
+	'click .createOrder': function(){
+		//window.alert("A new order has been created");
+		sampleOrder={
+		id: 123456787,
+        coupon_id:1001,
+        member_id:1001,
+        number_of_diner: 3,
+        created_date: new Date ("03/01/2016"),
+        coupon_id: 1001,
+        payable_amount: 50.00,
+        paying_method: "member card",
+        dishes: [
+            {
+                dish_id: 1001,
+                quantity: 1
+            },
+            {
+                dish_id: 2001,
+                quantity: 2
+            }
+        ]
+    };
+    createdOrderId = Orders.insert(sampleOrder);
+    console.log(createdOrderId);
+    customer=Members.findOne({member_id: sampleOrder.member_id});
+    id=customer._id;
+    Members.update({ _id: id },{ $push: { order_history: {order_id: sampleOrder.id }}});
+	}
+})
--- server/main.js
+++ server/main.js
@@ -2,4 +2,24 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+//define schema for the databases
+DishInfo.schema = new SimpleSchema({
+	dish_id: {type: Number},
+	quantity: {type: Number, defaultValue: 1}
+})
+
+Orders.schema = new SimpleSchema({
+	coupon_id: {type: Number, optional: true},
+	member_id: {type: Number, optional: true},
+	number_of_diner: {type: Number, defaultValue: 1},
+	created_date: {type: Date},
+	payable_amount: {type: Number},
+	paying_method: {type: String},
+	dishes: {type: [DishInfo.schema]}
 });
+
+});
+
+
+//insert two sample data into the databases
