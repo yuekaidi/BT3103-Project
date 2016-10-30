88be57b714524abbe219cbdb0280ad4f3a3184cd
yuekaidi <kaidi.yue@gmail.com>
2016-10-30 15:54:56 +0800

    add outlets

---
+++ client/outlets.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Mongo } from 'meteor/mongo';
+import { Session } from 'meteor/session';
+import { Order, Member, Coupon, Menu } from '../api/database.js';
+
+import './outlets.html';
